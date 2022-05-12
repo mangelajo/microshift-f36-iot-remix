abf87201be8fc91d13cc845fc6e3971e1c9cdac1
Miguel Angel Ajo <majopela@redhat.com>
2022-05-12 15:28:09 +0200

    dploy-repo

--- script.sh
+++ script.sh
@@ -9,5 +9,5 @@ cp ./custom/* .tmp/
 cat "./.fedora-iot-spec/treecompose-post.sh" "./custom/treecompose-post.sh" > ".tmp/treecompose-post.merged.sh"
 chmod +x ".tmp/treecompose-post.merged.sh"
 sudo rpm-ostree compose tree --unified-core --cachedir=".cache" --repo=".build-repo" --write-commitid-to="commit-file" ".tmp/treefile.json"
-
+sudo sudo ostree --repo=".deploy-repo" pull-local ".build-repo" "microshift/stable/x86_64"
 
